52a607f35c7d5a4bacdd4db5da45390329ce2510
Felipe <Felipe>
2019-09-30 19:37:35 -0300

    Lista de tarefas

--- git/lista/lib/home.dart
+++ git/lista/lib/home.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:lista/task.dart';
-import 'package:lista/task.dart' as prefix0;
+import 'package:lista/task.dart';
 
 class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
+  void _apaga() {
+    setState(() {
+      tarefas.removeWhere((tarefa) => tarefa.feito);
+    });
+  }
+
+  void _adiciona() {
+    setState(() {
+      tarefas.add(Task(msg: "", feito: false));
+    });
+  }
+
   List<Task> tarefas = [Task()];
 
   void initState() {
@@ -20,8 +33,7 @@ class _HomeState extends State<Home> {
         title: Text("Tarefas"),
         actions: <Widget>[
           IconButton(
-            onPressed: () {
-            },
+            onPressed:_apaga,
             icon: Icon(
               Icons.clear_all,
               size: 30,
@@ -29,46 +41,54 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Container(
-        color: Color(0xff5E5E5E),
-        padding: EdgeInsets.fromLTRB(16, 16, 16, 100),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: tarefas.map((Task task) {
-              return Row(
-                children: <Widget>[
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        task.feito = !task.feito;
-                      });
-                    },
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    icon: Icon(
-                      task.feito
-                          ? Icons.radio_button_checked
-                          : Icons.radio_button_unchecked,
-                      size: 35,
+      
+      body: SingleChildScrollView(
+        child: Container(
+          color: Color(0xff5E5E5E),
+          padding: EdgeInsets.fromLTRB(16, 16, 16, 100),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: tarefas.map((Task task) {
+                return Row(
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          task.feito = !task.feito;
+                        });
+                      },
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      icon: Icon(
+                        task.feito
+                            ? Icons.radio_button_checked
+                            : Icons.radio_button_unchecked,
+                        size: 35,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: TextField(),
-                  )
-                ],
-              );
-            }).toList()),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "O que precisa ser feito?",
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              }).toList()),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            tarefas.add(Task());
+            _adiciona();
           });
         },
         child: Icon(
           Icons.add,
         ),
       ),
+      backgroundColor: Color(0xff5E5E5E),
     );
   }
 }
--- git/lista/lib/task.dart
+++ git/lista/lib/task.dart
@@ -2,4 +2,7 @@ class Task{
   bool feito=false;
   String msg="";
 
+
+  Task({this.msg : "", this.feito : false,});
+
 }
